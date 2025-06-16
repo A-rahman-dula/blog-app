1569162c4decfe2c0da9abd94cc1043ef544c638
A-rahman-dula <arahmandulapandan@gmail.com>
2025-06-16 08:45:23 +0530

    set up Redux store with persistence and integrate it into the main application
    -configureStore from redux toolkit
    -persistStore from redux persist
    -custom middlewares
    -Provide gives redux access to entire app
    -Persisitgae delays rendering app untill persisted state is rehydrated

---
+++ src/store/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore non-serializable values from redux-persist
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
